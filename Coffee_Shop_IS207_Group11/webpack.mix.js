9f31ba471ff3ab08c2e115a9bad99e1ab5d27f2b
Hung Tuan Nguyen <hungtuan.onzo@gmail.com>
2023-11-13 15:27:50 +0700

    Fix laravel-mix compiler

--- Coffee_Shop_IS207_Group11/webpack.mix.js
+++ Coffee_Shop_IS207_Group11/webpack.mix.js
@@ -1,46 +1,59 @@
 const mix = require("laravel-mix");
+const glob = require("glob");
+const path = require("path");
 
-mix.js("resources/js/app.js", "public/assets/js").sass(
-    "resources/sass/app.scss",
-    "public/assets/css"
-);
+mix.options({
+    resourceRoot: process.env.ASSET_URL || undefined,
+    processCssUrls: false,
+    postCss: [require("autoprefixer")],
+});
 
 /*
  |--------------------------------------------------------------------------
  | Assets
  |--------------------------------------------------------------------------
- */
-
+*/
 function mixAssetsDir(query, cb) {
-    (glob.sync("resources/assets/" + query) || []).forEach((f) => {
-        f = f.replace(/[\\\/]+/g, "/");
-        cb(f, f.replace("resources/assets/", "public/assets/"));
-    });
+    const SRC = "resources/assets/";
+    const DEST = "public/assets/";
+
+    try {
+        const files = glob.sync(SRC + query);
+        files.forEach((f) => {
+            f = f.replace(/[\\\/]+/g, "/");
+            const destPath = f.replace(SRC, DEST);
+
+            cb(f, destPath);
+        });
+
+        console.log(`Successfully processed ${files.length} files.`);
+    } catch (error) {
+        console.error("Error:", error.message);
+    }
 }
 
 /*
-   |--------------------------------------------------------------------------
-   | Configure sass
-   |--------------------------------------------------------------------------
-   */
-
+  |--------------------------------------------------------------------------
+  | Configure sass
+  |--------------------------------------------------------------------------
+*/
 const sassOptions = {
     precision: 5,
 };
 
 // Core stylesheets
-mixAssetsDir("vendor/scss/**/!(_)*.scss", (src, dest) =>
+mixAssetsDir("sass/**/!(_)*.scss", (src, dest) =>
     mix.sass(
         src,
         dest
-            .replace(/(\\|\/)scss(\\|\/)/, "$1css$2")
+            .replace(/(\\|\/)sass(\\|\/)/, "$1css$2")
             .replace(/\.scss$/, ".css"),
         { sassOptions }
     )
 );
 
 // Core javascripts
-mixAssetsDir("vendor/js/**/*.js", (src, dest) => mix.js(src, dest));
+mixAssetsDir("js/**/*.js", (src, dest) => mix.js(src, dest));
 
 /*
  |--------------------------------------------------------------------------
@@ -51,6 +64,6 @@ mixAssetsDir("vendor/js/**/*.js", (src, dest) => mix.js(src, dest));
  | You may enable support for this by calling the mix.browserSync() method:
  | Make Sure to run `php artisan serve` and `yarn watch` command to run Browser Sync functionality
  | Refer official documentation for more information: https://laravel.com/docs/10.x/mix#browsersync-reloading
- */
+*/
 
 mix.browserSync("http://127.0.0.1:8000/");
