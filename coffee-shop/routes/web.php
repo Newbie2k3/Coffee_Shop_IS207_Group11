aac30f20a43e8b1408b705da4d16774e7126f720
Hung Tuan Nguyen <hungtuan.onzo@gmail.com>
2023-11-18 13:00:03 +0700

    Admin User with Middleware

--- coffee-shop/routes/web.php
+++ coffee-shop/routes/web.php
@@ -18,9 +18,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'verified', 'isAdmin']], function () {
+    Route::view('/', 'dashboard')->name('dashboard');
+});
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
