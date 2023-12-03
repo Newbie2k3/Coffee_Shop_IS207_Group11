0149b05cc9e4af149e8a90aeb95fe8c14aa5eb3f
Hung Tuan Nguyen <hungtuan.onzo@gmail.com>
2023-12-03 12:44:00 +0700

    Assign designing about us page task to Phuc

--- coffee-shop/resources/views/pages/about/index.blade.php
+++ coffee-shop/resources/views/pages/about/index.blade.php
@@ -1,7 +1,13 @@
 <x-guest-layout>
     @section('title', $title)
+    @section('page-style')
+        <!-- Menu -->
+        <link rel="stylesheet" href="{{ asset('assets/css/pages/phucdeptrai_about.css') }}">
+    @endsection
 
     <x-big-banner :imgUrl="'assets/img/backgrounds/home-bg.jpg'">
         <h1>About Us</h1>
     </x-big-banner>
+
+    @include('pages.about.phucdeptrai')
 </x-guest-layout>
