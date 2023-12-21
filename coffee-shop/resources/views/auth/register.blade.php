004309dca649b4c25c76710a38b9dd2a63a7af49
hoangphuc688 <104003926+hoangphuc688@users.noreply.github.com>
2023-12-21 14:16:02 +0700

    Revert "create layout login regis"
    This reverts commit 79f8d16f37aaba928c2da4b4ad214fa2f1df2fd8.

--- coffee-shop/resources/views/auth/register.blade.php
+++ coffee-shop/resources/views/auth/register.blade.php
@@ -5,14 +5,14 @@
         <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block w-full mt-1" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block w-full mt-1" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -20,7 +20,7 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block w-full mt-1"
+            <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
                             name="password"
                             required autocomplete="new-password" />
@@ -32,25 +32,21 @@
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-            <x-text-input id="password_confirmation" class="block w-full mt-1"
+            <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
                             name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="mt-4 ">
-            <div class="flex justify-center mr-6">
+        <div class="flex items-center justify-end mt-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                {{ __('Already registered?') }}
+            </a>
+
             <x-primary-button class="ms-4">
                 {{ __('Register') }}
             </x-primary-button>
-            </div>
-            <div class="flex justify-center mt-3">
-            <a class="text-sm text-gray-600 underline rounded-md hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-            </div>
-            
         </div>
     </form>
 </x-auth-layout>
--- coffee-shop/resources/views/layouts/auth.blade.php
+++ coffee-shop/resources/views/layouts/auth.blade.php
@@ -1,16 +1,13 @@
 <x-app-layout>
-        
-            <div class="flex flex-col items-center min-h-screen pt-6 bg-gray-100 bg-img max-w-screen sm:justify-center sm:pt-0 ">
-                <div>
-                    <a href="/">
-                        <x-application-logo class="w-20 h-20 text-gray-500 fill-current" />
-                    </a>
-                </div>
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div>
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </div>
 
-                <div class="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md hightlight sm:max-w-md sm:rounded-lg">
-                    {{ $slot }}
-                </div>
-            </div>
-            
-        
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            {{ $slot }}
+        </div>
+    </div>
 </x-app-layout>
