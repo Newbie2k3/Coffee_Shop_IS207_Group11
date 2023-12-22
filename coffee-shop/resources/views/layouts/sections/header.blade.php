5c89f63302921ce31c90e3de3549aa6e32381da6
Hung Tuan Nguyen <hungtuan.onzo@gmail.com>
2023-12-22 21:33:41 +0700

    Add responsive design using Alpine.js and Bootstrap

--- coffee-shop/resources/views/layouts/sections/header.blade.php
+++ coffee-shop/resources/views/layouts/sections/header.blade.php
@@ -1,10 +1,10 @@
-<header class="p-3 text-white">
-    <div class="container">
+<header x-data="{ open: false }" class="py-3 text-white">
+    <div class="container px-3">
         <div class="overlay"></div>
-        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between">
+        <div class="d-flex flex-wrap align-items-center justify-content-between">
             <a class="navbar-brand" href="{{ route('home') }}">Coffee<small>Shop</small></a>
 
-            <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <ul class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex nav justify-content-center">
                 <li class="nav-item {{ request()->is('great-deals') ? 'active' : '' }}">
                     <a class="nav-link px-3" href="{{ route('great-deals') }}">Khuyến mãi</a>
                 </li>
@@ -16,7 +16,7 @@
                 </li>
             </ul>
 
-            <div class="d-flex">
+            <div class="hidden sm:-my-px sm:ms-10 sm:flex">
                 {{-- Account --}}
                 <div class="icon-container">
                     <x-icon-modal menuOpen="accountMenuOpen" icon='<i class="fa-solid fa-user"></i>' size='small'>
@@ -91,6 +91,92 @@
                     @endauth
                 </div>
             </div>
+
+            <div class="-me-2 flex items-center sm:hidden">
+                <button @click="open = ! open"
+                    class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
+                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
+                            stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden bg-white mt-3">
+        <div class="p-4 text-black">
+            @if (Route::has('login'))
+                <div id="user-avatar" data-auth="{{ Auth::check() ? 'true' : 'false' }}"></div>
+                @auth
+                    <h3 class="modal-title text-black mb-3">{{ __(Auth::user()->name) }}
+                        <br>
+                        <span class="modal-subtitle">
+                            @admin
+                                Admin
+                            @else
+                                Guest
+                            @endadmin
+                        </span>
+                    </h3>
+
+                    <div class="d-flex gap-4 mb-1">
+                        @admin
+                            <a href="{{ route('dashboard.index') }}">Dashboard</a>
+                        @endadmin
+
+                        <a href="{{ route('profile.edit') }}">
+                            Profile
+                        </a>
+
+                        <a href="{{ route('payment.history') }}">
+                            Personal Invoices
+                        </a>
+
+                        <a href="{{ route('cart') }}">
+                            Your Cart
+                        </a>
+                    </div>
+
+                    <!-- Authentication -->
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <a href="route('logout')"
+                            onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </a>
+                    </form>
+                @else
+                    <h3 class="modal-title">Coffee Shop</h3>
+                    <a href="{{ route('login') }}">Log in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}">Register</a>
+                    @endif
+                @endauth
+            @endif
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('great-deals')" :active="request()->routeIs('great-deals')">
+                {{ __('Khuyến mãi') }}
+            </x-responsive-nav-link>
+        </div>
+
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('menu')" :active="request()->routeIs('menu')">
+                {{ __('Thực đơn') }}
+            </x-responsive-nav-link>
+        </div>
+
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('about')" :active="request()->routeIs('about')">
+                {{ __('Về chúng tôi') }}
+            </x-responsive-nav-link>
         </div>
     </div>
 </header>
--- coffee-shop/resources/views/pages/about/phucdeptrai.blade.php
+++ coffee-shop/resources/views/pages/about/phucdeptrai.blade.php
@@ -1,49 +1,52 @@
-<section class="flex">
-    <div>
-        <img width="auto"
-            src="https://raw.githubusercontent.com/letuyen2102/coffee_shop/letuyen-21522778/public/images/bg_2.jpg"
-            alt="">
-    </div>
-    <div class="form bg-black py-4">
-        <p class="mt-6 text-xl font-semibold text-white dark:text-white px-4">Tư vấn ngay<span id="loading-indicator"
-                style="display: none; margin-left:4px;">đang gửi...</span></p>
-        <form class="my-form px-4" id="leads-form">
-            <div class="row">
-                <div class="col">
-                    <input type="text" id='name' name='name' placeholder="Họ và tên" required>
+<section class="container p-0 m-0">
+    <div class="row">
+        <div class="col-12 col-md-5 p-0">
+            <img style="height:100%; object-fit: cover;"
+                src="https://raw.githubusercontent.com/letuyen2102/coffee_shop/letuyen-21522778/public/images/bg_2.jpg"
+                alt="Form Image">
+        </div>
+        <div class="col-12 col-md-7 bg-black py-5 pe-5">
+            <p class="text-xl font-semibold text-white dark:text-white px-4">Tư vấn ngay<span id="loading-indicator"
+                    style="display: none; margin-left:4px;">đang gửi...</span></p>
+            <form class="my-form px-4" id="leads-form">
+                <div class="row" width="100%">
+                    <div class="col-12 col-md-6">
+                        <input type="text" id='name' name='name' placeholder="Họ và tên" required>
 
+                    </div>
+                    <div class="col">
+                        <input type="text" id='email' name='email' placeholder="Email" required>
+                    </div>
                 </div>
-                <div class="col">
-                    <input type="text" id='email' name='email' placeholder="Email" required>
+                <div class="row">
+                    <div class="col">
+                        <input type="tel" id="phone" name="phone" placeholder="Số điện thoại" required>
+                    </div>
+                    <div class="col">
+                        <input type="text" id="address" name="address" placeholder="Địa chỉ" required>
+                    </div>
+                    <div>
+                        <input type="text" id='desc' name='desc' placeholder="Để lại lời nhắn">
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <input type="tel" id="phone" name="phone" placeholder="Số điện thoại" required>
+                <div class="form-group mt-2 my-form">
+                    <label for="product">Sản phẩm quan tâm:</label>
+                    <select class="product" id="product" name="product" required>
+                        <option value="">Chọn sản phẩm</option>
+                        @foreach ($product as $item)
+                            <option value="{{ $item->name }}">{{ $item->name }}</option>
+                        @endforeach
+                    </select>
                 </div>
-                <div class="col">
-                    <input type="text" id="address" name="address" placeholder="Địa chỉ" required>
-                </div>
-                <div>
-                    <input type="text" id='desc' name='desc' placeholder="Để lại lời nhắn">
-                </div>
-            </div>
-            <div class="form-group mt-2 my-form">
-                <label for="product">Sản phẩm quan tâm:</label>
-                <select class="product" id="product" name="product" required>
-                    <option value="">Chọn sản phẩm</option>
-                    @foreach ($product as $item)
-                        <option value="{{ $item->name }}">{{ $item->name }}</option>
-                    @endforeach
-                </select>
-            </div>
 
-            {{-- <input type="submit" value="Gửi ngay"> --}}
-            <div class="form-group d-flex mt-5">
-                <button id="submit-btn" type="button" class="btn btn-primary flex-grow-1">Gửi ngay</button>
-            </div>
-        </form>
+                {{-- <input type="submit" value="Gửi ngay"> --}}
+                <div class="form-group d-flex mt-5">
+                    <button id="submit-btn" type="button" class="btn btn-primary flex-grow-1 py-3 py-md-2">Gửi ngay</button>
+                </div>
+            </form>
+        </div>
     </div>
+
     @section('page-script')
         <script>
             const csrfToken = "{{ csrf_token() }}";
